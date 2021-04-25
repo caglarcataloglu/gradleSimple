0820e1d3273f942d085264fe4d05632e65a0e11e
crozwise <caglarcataloglu@gmail.com>
2021-04-25 18:27:26 +0300

    first commit

---
+++ src/main/java/com/caglar/MyApplication.java
@@ -0,0 +1,7 @@
+package com.caglar;
+
+public class MyApplication {
+    public static void main(String[] args) {
+        System.out.println("It workssss!!!");
+    }
+}
---
+++ src/test/java/com/caglar/MyApplicationTest.java
@@ -0,0 +1,10 @@
+package com.caglar;
+
+import org.junit.Test;
+
+public class MyApplicationTest {
+    @Test
+    public void testSomething(){
+        MyApplication.main(new String[]{});
+    }
+}
